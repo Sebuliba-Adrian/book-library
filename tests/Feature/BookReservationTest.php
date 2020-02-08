75d984b9f137611ff721ae6fc2b005b9f4abadf9
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2020-02-08 21:51:14 +0300

    add ability to add book to library

---
+++ tests/Feature/BookReservationTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookReservationTest extends TestCase
+{
+   use RefreshDatabase;
+   public function test_a_book_can_be_added_to_the_library(){
+
+       $this->withoutExceptionHandling();
+
+       $response = $this->post('/books', [
+         'title' => 'Cool Book Title',
+         'author' => 'Victor',
+       ]);
+       $response->assertOk();
+       $this->assertCount(1, Book::all());
+   }
+}
